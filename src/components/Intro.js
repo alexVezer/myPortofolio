806b05c452f800e0392c7b787f801f2fb1ab646b
AlexV <alexartimon@gmail.com>
2021-08-13 13:42:26 +0300

    polishing up

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ function Intro() {
         <div className="intro">
         <h1>Hi, I am Alex</h1>
 
-        <h1>I am a junior React developer.</h1>
+        <h1>I am a Jr. React developer.</h1>
         <h1 className="subtitle">Welcome to my playground!</h1> 
         
         </div>
